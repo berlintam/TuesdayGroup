f6c3b98377d4bd7b08ace77b2ad15cf5dd739c89
BerlinTam <tam.eastley@gmail.com>
2013-04-30 20:53:23 +0200

    add address

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,3 @@
 class Place < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :address
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130409184119) do
+ActiveRecord::Schema.define(:version => 20130430181216) do
 
   create_table "places", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "address"
   end
 
 end
